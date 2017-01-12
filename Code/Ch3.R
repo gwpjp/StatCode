28591067aaa47a5c64b3c03b3d6dc35c6e127c09
John Pearson <jpearson@John-Pearson-MacBook-Air.local>
2017-01-12 13:01:10 -0500

    Added a bunch of graphs for Ch. 3.

--- Code/Ch3.R
+++ Code/Ch3.R
@@ -6,7 +6,6 @@ ggplot(mtcars,aes(x=cyl,fill=factor(gear))) + geom_histogram(binwidth = 1,positi
 
 #Ex 1
 library(xlsx)
-setwd("~/Google Drive/Teaching(PaceOld)/16-17/Adv. Math/StatCode")
 ex1 <- read.xlsx("Ex3_1.xlsx","Sheet1") #Read in the sheet naively
 ex1 #This is not right!
 ex1 <- read.xlsx("Ex3_1.xlsx","Sheet2") #Read in only a data sheet
@@ -26,22 +25,34 @@ ex1v2
 dcast(ex1v2, Ideal.Distance ~ variable)
 #A bar graph with stacked bars
 ggplot(ex1v2,aes(x=Ideal.Distance,y=value,fill=variable)) + geom_bar(stat="identity")
+prefix <- "../Graphs/Ch3/Ex1/"
+ggsave(filename = paste(prefix,"1-SimpleBarGraphStacked.jpeg",sep = ""))
 #A bar graph with side-by-side bars
 ggplot(ex1v2,aes(x=Ideal.Distance,y=value,fill=variable)) + geom_bar(stat="identity",position="dodge")
+ggsave(filename = paste(prefix,"2-SimpleBarGraphSide.jpeg",sep = ""))
 #A nicer looking bar graph
 ggplot(ex1v2,aes(x=Ideal.Distance,y=value,fill=variable)) + geom_bar(stat="identity",position="dodge") +
   scale_fill_discrete(name="Respondent",breaks=c("Students","Parents"),labels=c("Student","Parent")) + 
   labs(x="Ideal Distance", y="Frequency",title="How Far is Far Enough?") +
   theme(plot.title = element_text(hjust = 0.5))
+ggsave(filename = paste(prefix,"3-NiceBarGraph.jpeg",sep = ""))
 #Nice looking, but compact
 ggplot(ex1v2,aes(x=Ideal.Distance,y=value,fill=variable)) + geom_bar(stat="identity",position="dodge") +
   scale_fill_discrete(name="Respondent",breaks=c("Students","Parents"),labels=c("Student","Parent")) + 
   labs(x="Ideal Distance", y="Frequency",title="How Far is Far Enough?") +
   theme(plot.title = element_text(hjust = 0.5),legend.position=c(.9,.9))
-#Faceting
-ggplot(ex1v2,aes(x=Ideal.Distance,y=value)) + geom_bar(stat="identity",position="dodge")  + 
+ggsave(filename = paste(prefix,"4-NiceBarGraph(Compact).jpeg",sep = ""))
+#Faceting w/ colors as before
+ggplot(ex1v2,aes(x=Ideal.Distance,y=value,fill=variable)) + geom_bar(stat="identity",position="dodge")  + 
   labs(x="Ideal Distance", y="Frequency",title="How Far is Far Enough?") +
-  theme(plot.title = element_text(hjust = 0.5))
+  theme(plot.title = element_text(hjust = 0.5)) + facet_grid(facets = variable ~ .)
+ggsave(filename = paste(prefix,"5-NiceBarGraph(Facets).jpeg",sep = ""))
+#Faceting w/ new colors
+ggplot(ex1v2,aes(x=Ideal.Distance,y=value,fill=Ideal.Distance)) + geom_bar(stat="identity",position="dodge")  + 
+  labs(x="Ideal Distance", y="Frequency",title="How Far is Far Enough?") +
+  theme(plot.title = element_text(hjust = 0.5)) + facet_grid(facets = variable ~ .)
+ggsave(filename = paste(prefix,"6-NiceBarGraph(Facets).jpeg",sep = ""))
+
 
 #Creating a pie chart
 ex1v3 <- melt(ex1[,c(1,4:5)],"Ideal.Distance")
@@ -52,15 +63,19 @@ ggplot(ex1v3,aes(x=variable,y=value,fill=Ideal.Distance)) + geom_bar(stat="ident
   labs(x="Respondent",y="Relative Frequency",title="How Far is Far Enough?") +
   theme(plot.title = element_text(hjust = 0.5)) + 
   scale_fill_discrete(name="Ideal Distance") 
+ggsave(filename = paste(prefix,"7-NiceBarGraph(RF).jpeg",sep = ""))
 #Do the same thing by faceting since we will want 2 pie charts
 ggplot(ex1v3,aes(x=factor(""),y=value,fill=Ideal.Distance)) + geom_bar(stat="identity") + 
   labs(x="",y="Relative Frequency",title="How Far is Far Enough?") +
   theme(plot.title = element_text(hjust = 0.5), axis.ticks = element_blank()) + 
   scale_fill_discrete(name="Ideal Distance") +
   facet_grid(facets = ~ variable)
+ggsave(filename = paste(prefix,"8-NiceBarGraph(RF-Facets).jpeg",sep = ""))
+
 #Make pie chart
 ggplot(ex1v3,aes(x=factor(""),y=value,fill=Ideal.Distance)) + geom_bar(stat="identity") + coord_polar(theta="y") +
   labs(x="",y="Relative Frequency",title="How Far is Far Enough?") +
   theme(plot.title = element_text(hjust = 0.5)) + 
   scale_fill_discrete(name="Ideal Distance") +
   facet_grid(facets = ~ variable) 
+ggsave(filename = paste(prefix,"9-PieChart(Facets).jpeg",sep = ""))
